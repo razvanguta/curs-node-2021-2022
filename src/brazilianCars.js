fbc027fb358fd47af4cdf0eb98fd3bb2ee97b3ed
razvanguta <razvanguta20101999@gmail.com>
2021-10-20 13:07:49 +0300

    tema api call brazilian cars update

--- src/brazilianCars.js
+++ src/brazilianCars.js
@@ -3,7 +3,11 @@ const fetch = require('node-fetch');
 const brazilianCars = async (req, res) => {
   const response = await fetch('https://parallelum.com.br/fipe/api/v1/carros/marcas');
   const body = await response.json();
-  res.send(body);
+  const facts = body.map(({ nome, codigo}) => ({
+    nome,
+    codigo,
+  }));
+  res.send(facts);
 }
 
 module.exports = brazilianCars;
