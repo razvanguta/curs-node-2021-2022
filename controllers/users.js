31d962d93978e80d02e2a92f7346bb8c202a2215
razvanguta <razvanguta20101999@gmail.com>
2021-10-27 21:45:43 +0300

    tema

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const db = require('../models');
+const user = require('../models/user');
 
 module.exports.getAllUsers = (req,res)=> {
     const allUsers = db.User.findAll(). then(response=>{
@@ -33,14 +34,63 @@ module.exports.createUser = async (req,res)=> {
 }
 }
 
-module.exports.getUserById = (req,res)=> {
-    
+module.exports.getUserById = async ({params},res)=> {
+    try{
+    const key = params.id;
+    const search = await db.User.findByPk(key);
+    if (search === null) {
+    res.send("Not found!")
+    } else {
+        res.send(search);
+    }
+    } catch(error){
+        res.send({
+            error:"Something went wrong"
+        })      
+    }
 }
 
-module.exports.updateUser = (req,res)=> {
-    
+module.exports.updateUser = async (req,res)=> {
+    const id = req.params.id
+    const {
+        email,
+        firstName,
+        lastName,
+    } = req.body
+    console.log(id+email)
+    try{
+    await db.User.update({
+        email,
+        firstName,
+        lastName,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+    where:{
+        id,
+    }
+    });
+    res.send(await db.User.findByPk(id));
+} catch(error){
+    res.send({
+        error:"Something went wrong"
+    })
+}
 }
 
-module.exports.deleteUser = (req,res)=> {
-    
+module.exports.deleteUser = async (req,res)=> {
+    try{
+    const id = req.params.id
+    await db.User.destroy({
+        where: {
+          id,
+        }
+      });
+      res.send("Success")
+    } catch(error){
+        res.send({
+            error:"Something went wrong"
+        })
+    }
 }
