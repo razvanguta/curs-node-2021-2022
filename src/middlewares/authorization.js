e1d3b513aedff207b1aa4fe3eb0897de3088267c
razvanguta <razvanguta20101999@gmail.com>
2021-10-27 16:08:06 +0300

    toate modificarile

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -2,8 +2,8 @@ const authorizationMiddleware = (req, res, next) => {
     const authorization = req.headers.authorization;
     if(authorization){
       try{
-        console.log(authorization)
       const decoded = jwt.verify(authorization.replace('Baerer ',''),MY_SECRET_KEY);
+      
       } catch(e){
         res.send({
           error: "Invalid token",
