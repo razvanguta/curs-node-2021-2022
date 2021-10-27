38d975ac8506be2bd10cfdb88ed9d867228c5723
razvanguta <razvanguta20101999@gmail.com>
2021-10-27 17:31:04 +0300

    sqlite database

---
+++ controllers/users.js
@@ -0,0 +1,19 @@
+module.exports.getAllUsers = (req,res)=> {
+    
+}
+
+module.exports.getUserById = (req,res)=> {
+    
+}
+
+module.exports.createUser = (req,res)=> {
+    
+}
+
+module.exports.updateUser = (req,res)=> {
+    
+}
+
+module.exports.deleteUser = (req,res)=> {
+    
+}
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const handleGreeting = require('./controllers/greeting');
 const { port } = require('./config/express');
 const authorizationMiddleware = require('./middlewares/authorization');
 const loginHandler = require('./controllers/login');
 const brazilianCars = require('./brazilianCars');
+const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require('./controllers/users');
 const app = express();
 app.use(bodyParser.json());
+
+app.get("/users",getAllUsers)
+app.get("/users/:id",getUserById);
+app.post("/users",createUser);
+app.put("/users/:id",updateUser);
+app.delete("/users/:id",deleteUser);
+
 
 app.post("/login", loginHandler);
 
